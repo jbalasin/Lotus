b4ec4304ac4ba774b7e39dff8931b2d42f9dbc94
jbalasin <jbalasin@ucsc.edu>
2016-11-15 16:48:36 -0800

    Update ANN.scala

--- Lotus/src/Lotus/ANN.scala
+++ Lotus/src/Lotus/ANN.scala
@@ -2,12 +2,33 @@ package Lotus
 import scala.math
 
 class ANN {
+  
+  val data: Matrix = InputData
+
+  
   private class Node{
     
   }
   
   def sigmoid(x:Double) : Double = {
     1/(1 + math.exp(-1*x))
+  }
+  
+  
+  def build_Model(hiddenNodes:Int, iterations:Int = 20000,printLoss:Boolean = false) : Map[String,Matrix] = {
+    val r = scala.util.Random
+    val dimensions = data.getNumberOfColumns()
+    val lim = hiddenNodes * dimensions
+    var startingMat = 1 to lim map (_ => r.nextGaussian())
+    
+    var W1 = new Matrix(hiddenNodes,data.getNumberOfColumns(), startingMat.toList )
+    
+    var Model:Map[String,Matrix] = Map()
+    
+    
+    
+    
+    return Model
   }
   
 }
