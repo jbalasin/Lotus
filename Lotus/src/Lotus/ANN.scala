ceebb04ec1a3d1144987e763d7903b890e4737a7
jbalasin <jbalasin@ucsc.edu>
2016-11-16 16:52:53 -0800

    Update ANN.scala

--- Lotus/src/Lotus/ANN.scala
+++ Lotus/src/Lotus/ANN.scala
@@ -15,13 +15,15 @@ class ANN {
   }
   
   
-  def build_Model(hiddenNodes:Int, iterations:Int = 20000,printLoss:Boolean = false) : Map[String,Matrix] = {
+  def build_Model(hiddenNodes:Int, iterations:Int = 20000,printLoss:Boolean = false,outputDim:Int,inputDim:Int) : Map[String,Matrix] = {
     val r = scala.util.Random
-    val dimensions = data.getNumberOfColumns()
+    val dimensions = data.getNumberOfColumns() - 1
     val lim = hiddenNodes * dimensions
     var startingMat = 1 to lim map (_ => r.nextGaussian())
-    
-    var W1 = new Matrix(hiddenNodes,data.getNumberOfColumns(), startingMat.toList )
+    var startingMat2 = 1 to lim map (_ => r.nextGaussian())
+    var W1 = new Matrix(data.getNumberOfColumns(),inputDim, startingMat.toList ) / Math.sqrt(inputDim)
+    var b1 = new Vector(List.tabulate(hiddenNodes)(_ => 0.0))
+    var W2 = new Matrix(outputDim,data.getNumberOfColumns(),startingMat2.toList) / Math.sqrt(outputDim)
     
     var Model:Map[String,Matrix] = Map()
     
